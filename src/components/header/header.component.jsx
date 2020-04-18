50a22ecef16a1a1c34bb321df3b603a4316dbc0a
Carl <carl.agbenyega@gmail.com>
2020-04-18 15:09:46 -0600

    added cart dropdown components and redux information

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 
-import { auth } from './../../firebase/firebase.utils'
+import { auth } from './../../firebase/firebase.utils';
+import CartIcon from './../cart-icon/cart-icon.component';
+import CartDropdown from './../cart-dropdown/cart-dropdown.component';
 
-import { ReactComponent as Logo } from '../../assets/crown.svg'
+import { ReactComponent as Logo } from '../../assets/crown.svg';
 
 import './header.styles.scss';
 
@@ -25,7 +27,9 @@ const Header = ({ currentUser }) => (
                 <div className='option' onClick={() => auth.signOut() }>SIGN OUT</div>
                 : <Link className='option' to='/signin'>SIGN IN</Link>
             }
+            <CartIcon />
         </div>
+        <CartDropdown />
     </div>
 );
 
