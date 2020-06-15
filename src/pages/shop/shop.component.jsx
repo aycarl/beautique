7d35aa7607c75813d1f237b3f45c42144adfd8fb
Carl <carl.agbenyega@gmail.com>
2020-06-14 22:44:23 -0600

    created collection overview component to replace component in shop component

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,20 +1,12 @@
 import React from "react";
-import { connect } from "react-redux";
-import { createStructuredSelector } from "reselect";
 
-import CollectionPreview from "./../../components/collection-preview/collection-preview.component";
-import { selectShopCollections } from "../../redux/shop/shop.selectors";
+import CollectionOverview from "./../../components/collection-overview/collection-overview.component";
 
-const ShopPage = ({collections}) => (
+const ShopPage = () => (
   <div className="shop-page">
-    {collections.map(({ id, ...otherCollectionProps }) => (
-      <CollectionPreview key={id} {...otherCollectionProps} />
-    ))}
+    <CollectionOverview />
   </div>
 );
 
-const mapStateToProps = createStructuredSelector({
-    collections: selectShopCollections
-});
 
-export default connect(mapStateToProps)(ShopPage);
+export default ShopPage;
