919b42f82ef17697aad45247e6e9db3d388bf779
Carl <carl.agbenyega@gmail.com>
2020-07-27 22:43:07 -0600

    added rootSaga to redux store

--- src/redux/shop/shop.sagas.js
+++ src/redux/shop/shop.sagas.js
@@ -1,15 +1,37 @@
-import { takeEvery } from 'redux-saga/effects';
+import { takeEvery, call, put } from "redux-saga/effects";
 
-import { ShopActionTypes } from './shop.types';
+import { ShopActionTypes } from "./shop.types";
 
+import {
+  firestore,
+  convertCollectionsSnapshotToMap,
+} from "./../../firebase/firebase.utils";
+
+import {
+  fetchCollectionsSuccess,
+  fetchCollectionsFailure,
+} from "./shop.actions";
 
 export function* fetchCollectionsAsync() {
-  yield console.log('I am fired');
+  yield console.log("I am fired");
+
+  try {
+    const collectionRef = firestore.collection("collections");
+    const snapshot = yield collectionRef.get();
+    const collectionsMap = yield call(
+      convertCollectionsSnapshotToMap,
+      snapshot
+    );
+    yield put(fetchCollectionsSuccess(collectionsMap));
+  } catch (error) {
+    yield put(fetchCollectionsFailure(error.message))
+  }
 }
 
 export function* fetchCollectionsStart() {
+  yield console.log("I am fired 09128837562");
   yield takeEvery(
     ShopActionTypes.FETCH_COLLECTIONS_START,
     fetchCollectionsAsync
-  )
+  );
 }
